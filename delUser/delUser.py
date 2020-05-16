1390b2e75e7869c0f59eb7c07356a35e04fc717b
Erick Santizo <esantizo@octagt.com>
2020-05-15 21:21:04 -0600

    del user

---
+++ delUser/delUser.py
@@ -0,0 +1,64 @@
+import sys
+import logging
+import json
+import os
+
+import boto3
+import botocore.exceptions
+from boto3.dynamodb.conditions import Key, Attr
+from dynamodb_json import json_util as json_dynamodb
+
+REGION = 'us-east-1'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+logger.info("SUCCESS: Connection to DynamoDB succeeded")
+
+def lambda_handler(event, context):
+    stage = event['headers']
+    if stage['origin'] != "http://localhost:4200":
+        cors = os.environ['prodCors']
+    else:
+        cors = os.environ['devCors']
+        
+    try:
+        statusCode = ''
+        userId = event['pathParameters']['id']
+        businessId = event['pathParameters']['businessId']
+        
+        table = dynamodb.Table('Cashier2Go')
+        response = table.update_item(
+            Key={
+                'PKID': 'BUS#' + businessId,
+                'SKID': 'USER#' + userId
+            },
+            UpdateExpression="set #s = :status",
+            ExpressionAttributeNames={
+                '#s': 'STATUS'
+            },
+            ExpressionAttributeValues={
+                ':status': 2
+            }
+            # ReturnValues="UPDATED_NEW"
+        )
+        statusCode = 200
+        body = json.dumps({'Message': 'User deleted successfully'})
+
+        if statusCode == '':
+            statusCode = 500
+            body = json.dumps({'Message': 'Error on delete user'})
+    except Exception as e:
+        statusCode = 500
+        body = json.dumps({'Message': 'Error on request try again'})  #str(e) })
+
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            "content-type" : "application/json",
+            "access-control-allow-origin" : cors
+        },
+        'body' : body
+    }
+    return response
