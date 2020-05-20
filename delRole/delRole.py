1379d62c5ea75249cd918c0b30b6f51f30676929
Erick Santizo <esantizo@octagt.com>
2020-05-19 20:29:23 -0600

    delete role

---
+++ delRole/delRole.py
@@ -0,0 +1,60 @@
+import sys
+import logging
+import json
+
+import boto3
+import botocore.exceptions
+from boto3.dynamodb.conditions import Key, Attr
+from dynamodb_json import json_util as json_dynamodb
+
+import os
+
+REGION = 'us-east-1'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+logger.info("SUCCESS: Connection to DynamoDB succeeded")
+
+def lambda_handler(event, context):
+    stage = event['headers']
+    if stage['origin'] != "http://localhost:4200":
+        cors = os.environ['prodCors']
+    else:
+        cors = os.environ['devCors']
+        
+    try:
+        data = json.loads(event['body'])
+        roleId = data['RoleId']
+        businessId = data['BusinessId']
+
+        e = {'#s': 'STATUS'}
+        table = dynamodb.Table('TuCita247')
+        response = table.update_item(
+            Key={
+                'PKID': 'BUS#' + businessId,
+                'SKID': 'ROL#' + roleId
+            },
+            UpdateExpression="set #s = :status",
+            ExpressionAttributeNames=e,
+            ExpressionAttributeValues={
+                ':status': {'N':'2'}
+            }
+        )
+        
+        statusCode = 200
+        body = json.dumps({'Message': 'Role updated successfully'})
+    except Exception as e:
+        statusCode = 500
+        body = json.dumps({'Message': 'Error on request try again ' + str(e)})
+
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            "content-type" : "application/json",
+            "access-control-allow-origin" : cors
+        },
+        'body' : body
+    }
+    return response
