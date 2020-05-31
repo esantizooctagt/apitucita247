6bced1f76577a3ea644754a95c6d32c1fc5cfbc6
Erick Santizo <esantizo@octagt.com>
2020-05-31 13:24:04 -0600

    put appointment

--- putAppointment/putAppointment.py
+++ putAppointment/putAppointment.py
@@ -7,6 +7,10 @@ import botocore.exceptions
 from boto3.dynamodb.conditions import Key, Attr
 from dynamodb_json import json_util as json_dynamodb
 
+import datetime
+import dateutil.tz
+from datetime import timezone
+
 import os
 
 REGION = 'us-east-1'
@@ -32,18 +36,23 @@ def lambda_handler(event, context):
         dateAppo = data['DateAppo']
         reasonId = data['Reason']
 
+        country_date = dateutil.tz.gettz('America/Puerto_Rico')
+        today = datetime.datetime.now(tz=country_date)
+        dateOpe = today.strftime("%Y-%m-%d-%H-%M-%S")
+
         table = dynamodb.Table('TuCita247')
         e = {'#s': 'STATUS'}
         if reasonId != '':
-            v = {':status': str(status), ':key01': str(status) + '#DT#' + str(dateAppo), ':reason': reasonId}
+            v = {':status': str(status), ':key01': str(status) + '#DT#' + str(dateAppo), ':reason': reasonId, ':dateope': dateOpe}
         else:
-            v = {':status': str(status), ':key01': str(status) + '#DT#' + str(dateAppo)}
+            v = {':status': str(status), ':key01': str(status) + '#DT#' + str(dateAppo), ':dateope': dateOpe}
+
         response = table.update_item(
             Key={
                 'PKID': 'APPO#' + appointmentId,
                 'SKID': 'APPO#' + appointmentId
             },
-            UpdateExpression="set #s = :status, GSI1SK = :key01, GSI2SK = :key01" + ", ReasonId = :reason" if reasonId != "" else "",
+            UpdateExpression="set #s = :status, GSI1SK = :key01, GSI2SK = :key01" + ", TIMECHEK = :dateope" if str(status) == "2" else "" + ", TIMECANCEL = :dateope" if str(status) == "5" else "" + ", TIMECHECKIN = :dateope" if str(status) == "3" else "" + ", ReasonId = :reason" if reasonId != "" else "",
             ExpressionAttributeNames=e,
             ExpressionAttributeValues=v
             # ReturnValues="UPDATED_NEW"
