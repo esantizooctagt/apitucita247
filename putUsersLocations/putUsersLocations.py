e74bbb98426ad5c011da81960d773769875c74d6
Erick Santizo <esantizo@octagt.com>
2020-06-05 15:59:26 -0600

    put user locs

---
+++ putUsersLocations/putUsersLocations.py
@@ -0,0 +1,62 @@
+import sys
+import logging
+import json
+
+import boto3
+import botocore.exceptions
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key, Attr
+from dynamodb_json import json_util as json_dynamodb
+
+import os
+
+REGION = 'us-east-1'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+logger.info("SUCCESS: Connection to DynamoDB succeeded")
+
+def lambda_handler(event, context):
+    stage = event['headers']
+    if stage['origin'] != "http://localhost:4200":
+        cors = os.environ['prodCors']
+    else:
+        cors = os.environ['devCors']
+        
+    try:
+        statusCode = ''
+        data = json.loads(event['body'])
+        businessId = event['pathParameters']['businessId']
+
+        for row in data['Users']:
+            table = dynamodb.Table('TuCita247')
+            response = table.update_item(
+                Key={
+                    'PKID': 'BUS#' + businessId,
+                    'SKID': 'USER#' + row['UserId']
+                },
+                UpdateExpression="set LOCATIONID = :locationId, DOOR = :door",
+                ExpressionAttributeValues={
+                    ':locationId': data['LocationId'], 
+                    ':door': data['Door']
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+
+        statusCode = 200
+        body = json.dumps({'Message': 'Users updated successfully', 'Code': 200})
+    except Exception as e:
+        statusCode = 500
+        body = json.dumps({'Message': 'Error on request try again ' + str(e), 'Code': 500})
+
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            "content-type" : "application/json",
+            "access-control-allow-origin" : cors
+        },
+        'body' : body
+    }
+    return response
