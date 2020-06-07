eaa4ed5586991e1a406934779a79445db01b104a
Erick Santizo <esantizo@octagt.com>
2020-06-06 21:21:06 -0600

    del polls

---
+++ delPoll/delPoll.py
@@ -0,0 +1,65 @@
+import sys
+import logging
+import json
+
+import boto3
+import botocore.exceptions
+from boto3.dynamodb.conditions import Key, Attr
+
+import base64
+
+import uuid
+import os
+
+REGION = 'us-east-1'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+logger.info("SUCCESS: Connection to DynamoDB succeeded")
+    
+def lambda_handler(event, context):
+    stage = event['headers']
+    if stage['origin'] != "http://localhost:4200":
+        cors = os.environ['prodCors']
+    else:
+        cors = os.environ['devCors']
+        
+    try:
+        statusCode = ''
+        pollId = event['pathParameters']['pollId']
+        businessId = event['pathParameters']['businessId']
+ 
+        table = dynamodb.Table('TuCita247')
+        response = table.update_item(
+            Key={
+                'PKID': 'BUS#' + businessId,
+                'SKID': 'POLL#' + pollId
+            },
+            UpdateExpression="SET #s :status",
+            ExpressionAttributeNames={'#s': 'STATUS'},
+            ExpressionAttributeValues={':status': 2},
+            ConditionExpression="attribute_exists(PKID) AND attribute_exists(SKID)",
+            ReturnValues="NONE"
+        )
+
+        statusCode = 200
+        body = json.dumps({'Message': 'Poll deleted successfully', 'Code': 200})
+
+        if statusCode == '':
+            statusCode = 500
+            body = json.dumps({'Message': 'Error on deleted poll'})
+    except Exception as e:
+        statusCode = 500
+        body = json.dumps({'Message': 'Error on request try again ' + str(e)})
+
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            "content-type" : "application/json",
+            "access-control-allow-origin" : cors
+        },
+        'body' : body
+    }
+    return response
