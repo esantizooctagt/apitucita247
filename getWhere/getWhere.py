360388b677b460334bf043f3f9267c407fc00b31
Erick Santizo <esantizo@octagt.com>
2020-06-09 23:14:33 -0600

    getWhere

---
+++ getWhere/getWhere.py
@@ -0,0 +1,79 @@
+import sys
+import logging
+import json
+
+import boto3
+import botocore.exceptions
+from boto3.dynamodb.conditions import Key, Attr
+from dynamodb_json import json_util as json_dynamodb
+
+import os
+
+REGION = 'us-east-1'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+dynamodb = boto3.client('dynamodb', region_name='us-east-1')
+logger.info("SUCCESS: Connection to DynamoDB succeeded")
+
+def lambda_handler(event, context):
+    stage = event['headers']
+    cors = stage['origin']
+
+    records =[]
+    try:
+        country = event['pathParameters']['country']
+
+        response = dynamodb.query(
+            TableName="TuCita247",
+            ReturnConsumedCapacity='TOTAL',
+            KeyConditionExpression='GSI1PK = :country',
+            ExpressionAttributeValues={
+                ':country': {'S': 'COUNTRY#' + country}
+            }
+        )
+        for row in json_dynamodb.loads(response['Items']):
+            recordset = {
+                'Parent': row['PKID'].replace('COUNTRY#' + country,''),
+                'City': row['SKID'],
+                'Name': row['NAME']
+            }
+            records.append(recordset)
+        
+        lastItem = ''
+        while 'LastEvaluatedKey' in response:
+            lastItem = json_dynamodb.loads(response['LastEvaluatedKey'])
+
+            response = dynamodb.query(
+                TableName="TuCita247",
+                ExclusiveStartKey= lastItem,
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI1PK = :country',
+                ExpressionAttributeValues={
+                    ':country': {'S': 'COUNTRY#' + country}
+                }
+            )
+            for row in json_dynamodb.loads(response['Items']):
+                recordset = {
+                    'Parent': row['PKID'].replace('COUNTRY#' + country,''),
+                    'City': row['SKID'],
+                    'Name': row['NAME']
+                }
+                records.append(recordset)
+        
+        statusCode = 200
+        body = json.dumps(records)
+    except Exception as e:
+        statusCode = 500
+        body = json.dumps({'Message': 'Error on request try again ' + str(e)})
+
+    response = {
+        'statusCode' : statusCode,
+        'headers' : {
+            "content-type" : "application/json",
+            "Access-Control-Allow-Origin" : cors
+        },
+        'body' : body
+    }
+    return response
