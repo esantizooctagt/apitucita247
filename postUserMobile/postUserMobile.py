4ec38bb3049eef5daef12fff9a41994c8d7c9350
Erick Santizo <esantizo@octagt.com>
2020-10-20 18:08:12 -0600

    post user mobile data

--- postUserMobile/postUserMobile.py
+++ postUserMobile/postUserMobile.py
@@ -38,40 +38,72 @@ def lambda_handler(event, context):
 
         items = []
         recordset = {}
-        recordset = {
-                "Put": {
-                    "TableName": "TuCita247",
-                    "Item": {
-                        "PKID": {"S": 'MOB#' + data['Phone'] },
-                        "SKID": {"S": 'CUS#' + customerId},
-                        "GSI1PK": {"S": 'CUS#' + customerId},
-                        "GSI1SK": {"S": 'CUS#' + customerId},
-                        "NAME": {"S": data['Name']},
-                        "EMAIL": {"S": str(data['Email']) if str(data['Email']) != '' else None},
-                        "DOB": {"S": str(data['DOB']) if str(data['DOB']) != '' else None},
-                        "GENDER": {"S": str(data['Gender']) if str(data['Gender']) != '' else None},
-                        "PREFERENCES": {"N": str(data['Preferences']) if str(data['Preferences']) != '' else None},
-                        "DISABILITY": {"N": str(data['Disability']) if str(data['Disability']) != '' else None},
-                        "PLAYERID": {"S": data['PlayerId'] if data['PlayerId'] != '' else None},
-                        "STATUS": {"N": "1"}
-                    },
-                    "ConditionExpression": "attribute_not_exists(PKID) AND attribute_not_exists(SKID)",
-                    "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
-                    }
-                }
-        items.append(cleanNullTerms(recordset))
 
-        recordset = {
-                "Put": {
+        if data['CustId'] != '':
+            oldRecord = {}
+            oldItems = []
+            oldRecord = {
+                "Delete": {
                     "TableName": "TuCita247",
                     "Item": {
                         "PKID": {"S": 'MOB#' + data['Phone']},
                         "SKID": {"S": 'MOB#' + data['Phone']}
                     },
-                    "ConditionExpression": "attribute_not_exists(PKID) AND attribute_not_exists(SKID)",
                     "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
                     }
                 }
+            oldItems.append(cleanNullTerms(oldRecord))
+
+            oldRecord = {
+                "Delete": {
+                    "TableName": "TuCita247",
+                    "Key": {
+                        "PKID": {"S": 'MOB#' + data['Phone']},
+                        "SKID": {"S": 'CUS#' + data['CustId']}
+                    },
+                    "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
+                    }
+                }
+            oldItems.append(cleanNullTerms(oldRecord))
+
+            responseOld = dynamodb.transact_write_items(
+                TransactItems = oldItems
+            )
+
+        recordset = {
+            "Put": {
+                "TableName": "TuCita247",
+                "Item": {
+                    "PKID": {"S": 'MOB#' + data['Phone'] },
+                    "SKID": {"S": 'CUS#' + customerId},
+                    "GSI1PK": {"S": 'CUS#' + customerId},
+                    "GSI1SK": {"S": 'CUS#' + customerId},
+                    "NAME": {"S": data['Name']},
+                    "EMAIL": {"S": str(data['Email']) if str(data['Email']) != '' else None},
+                    "DOB": {"S": str(data['DOB']) if str(data['DOB']) != '' else None},
+                    "GENDER": {"S": str(data['Gender']) if str(data['Gender']) != '' else None},
+                    "PREFERENCES": {"N": str(data['Preferences']) if str(data['Preferences']) != '' else None},
+                    "DISABILITY": {"N": str(data['Disability']) if str(data['Disability']) != '' else None},
+                    "PLAYERID": {"S": data['PlayerId'] if data['PlayerId'] != '' else None},
+                    "STATUS": {"N": "1"}
+                },
+                "ConditionExpression": "attribute_not_exists(PKID) AND attribute_not_exists(SKID)",
+                "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
+                }
+            }
+        items.append(cleanNullTerms(recordset))
+
+        recordset = {
+            "Put": {
+                "TableName": "TuCita247",
+                "Item": {
+                    "PKID": {"S": 'MOB#' + data['Phone']},
+                    "SKID": {"S": 'MOB#' + data['Phone']}
+                },
+                "ConditionExpression": "attribute_not_exists(PKID) AND attribute_not_exists(SKID)",
+                "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
+                }
+            }
         items.append(cleanNullTerms(recordset))
 
         response = dynamodb.transact_write_items(
