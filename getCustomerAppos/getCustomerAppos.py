fb69b4f5e26835487890f3da412861f6543ee001
Erick Santizo <esantizo@octagt.com>
2020-11-11 20:01:00 -0600

    old appos app mobile

--- getCustomerAppos/getCustomerAppos.py
+++ getCustomerAppos/getCustomerAppos.py
@@ -27,24 +27,41 @@ def getKey(obj):
 def lambda_handler(event, context):    
     try:
         customerId = event['pathParameters']['customerId']
+        typeAppo = int(event['pathParameters']['typeAppo'])
+
         country_date = dateutil.tz.gettz('America/Puerto_Rico')
         today = datetime.datetime.now(tz=country_date)
         dateOpe = today.strftime("%Y-%m-%d-00-00")
         newDate = (today + datetime.timedelta(days=365)).strftime("%Y-%m-%d-00-00")
         endToday = today.strftime("%Y-%m-%d-23-59")
+        dateYest = (today + datetime.timedelta(days=-1)).strftime("%Y-%m-%d-23-59")
 
         dateHoy = dateOpe
-        details = dynamodb.query(
-            TableName="TuCita247",
-            IndexName="TuCita247_CustAppos",
-            ReturnConsumedCapacity='TOTAL',
-            KeyConditionExpression='GSI2PK = :customerId AND GSI2SK between :dateHoy AND :newDate',
-            ExpressionAttributeValues={
-                ':customerId': {'S': 'CUS#' + customerId},
-                ':dateHoy': {'S': '1#DT#' + dateHoy},
-                ':newDate': {'S': '1#DT#' + newDate}
-            }
-        )
+        if typeAppo == 0:
+            details = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK between :dateHoy AND :newDate',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateHoy': {'S': '1#DT#' + dateHoy},
+                    ':newDate': {'S': '1#DT#' + newDate}
+                }
+            )
+        else:
+            details = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK BETWEEN :dateIni AND :dateYest',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateYest': {'S': '1#DT#' + dateYest},
+                    ':dateIni': {'S': '1#DT#'}
+                }
+            )
+
         recordset = {}
         record = []
         for item in json_dynamodb.loads(details['Items']):
@@ -130,17 +147,30 @@ def lambda_handler(event, context):
             }
             record.append(recordset)
 
-        precheckIn = dynamodb.query(
-            TableName="TuCita247",
-            IndexName="TuCita247_CustAppos",
-            ReturnConsumedCapacity='TOTAL',
-            KeyConditionExpression='GSI2PK = :customerId AND GSI2SK between :dateHoy AND :newDate',
-            ExpressionAttributeValues={
-                ':customerId': {'S': 'CUS#' + customerId},
-                ':dateHoy': {'S': '2#DT#' + dateHoy},
-                ':newDate': {'S': '2#DT#' + endToday}
-            }
-        )
+        if typeAppo == 0:
+            precheckIn = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK between :dateHoy AND :newDate',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateHoy': {'S': '2#DT#' + dateHoy},
+                    ':newDate': {'S': '2#DT#' + endToday}
+                }
+            )
+        else:
+            precheckIn = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK BETWEEN :dateIni AND :dateYest',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateYest': {'S': '2#DT#' + dateYest},
+                    ':dateIni': {'S': '2#DT#'}
+                }
+            )
         recordset = {}
         for item in json_dynamodb.loads(precheckIn['Items']):
             locs = dynamodb.query(
@@ -225,17 +255,30 @@ def lambda_handler(event, context):
             }
             record.append(recordset)
 
-        checkIn = dynamodb.query(
-            TableName="TuCita247",
-            IndexName="TuCita247_CustAppos",
-            ReturnConsumedCapacity='TOTAL',
-            KeyConditionExpression='GSI2PK = :customerId AND GSI2SK between :dateHoy AND :newDate',
-            ExpressionAttributeValues={
-                ':customerId': {'S': 'CUS#' + customerId},
-                ':dateHoy': {'S': '3#DT#' + dateHoy},
-                ':newDate': {'S': '3#DT#' + endToday}
-            }
-        )
+        if typeAppo == 0:
+            checkIn = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK between :dateHoy AND :newDate',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateHoy': {'S': '3#DT#' + dateHoy},
+                    ':newDate': {'S': '3#DT#' + endToday}
+                }
+            )
+        else:
+            checkIn = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK BETWEEN :dateIni AND :dateYest',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateYest': {'S': '3#DT#' + dateYest},
+                    ':dateIni': {'S': '3#DT#'}
+                }
+            )
         recordset = {}
 
         for item in json_dynamodb.loads(checkIn['Items']):
@@ -320,6 +363,102 @@ def lambda_handler(event, context):
                 'ProvName': provName
             }
             record.append(recordset)
+
+        if typeAppo == 1:
+            checkOut = dynamodb.query(
+                TableName="TuCita247",
+                IndexName="TuCita247_CustAppos",
+                ReturnConsumedCapacity='TOTAL',
+                KeyConditionExpression='GSI2PK = :customerId AND GSI2SK BETWEEN :dateIni AND :dateYest',
+                ExpressionAttributeValues={
+                    ':customerId': {'S': 'CUS#' + customerId},
+                    ':dateYest': {'S': '4#DT#' + dateYest},
+                    ':dateIni': {'S': '4#DT#'}
+                }
+            )
+            recordset = {}
+            for item in json_dynamodb.loads(checkOut['Items']):
+                locs = dynamodb.query(
+                    TableName="TuCita247",
+                    ReturnConsumedCapacity='TOTAL',
+                    KeyConditionExpression='PKID = :businessId AND SKID = :locationId',
+                    ExpressionAttributeValues={
+                        ':businessId': {'S': 'BUS#' + item['GSI1PK'].split('#')[1]},
+                        ':locationId': {'S': 'LOC#' + item['GSI1PK'].split('#')[3] }
+                    },
+                    Limit = 1
+                )
+                for locations in json_dynamodb.loads(locs['Items']):
+                    Address = locations['ADDRESS']
+
+                bus = dynamodb.query(
+                    TableName="TuCita247",
+                    ReturnConsumedCapacity='TOTAL',
+                    KeyConditionExpression='PKID = :businessId AND SKID = :meta',
+                    ExpressionAttributeValues={
+                        ':businessId': {'S': 'BUS#' + item['GSI1PK'].split('#')[1]},
+                        ':meta': {'S': 'METADATA' }
+                    },
+                    Limit = 1
+                )
+                for business in json_dynamodb.loads(bus['Items']):
+                    Name = business['NAME']
+
+                servs = dynamodb.query(
+                    TableName="TuCita247",
+                    ReturnConsumedCapacity='TOTAL',
+                    KeyConditionExpression='PKID = :businessId AND begins_with(SKID , :serv)',
+                    ExpressionAttributeValues={
+                        ':businessId': {'S': 'BUS#' + item['GSI1PK'].split('#')[1]},
+                        ':serv': {'S': 'SER#' }
+                    }
+                )
+                count = 0
+                servName = ''
+                for serv in json_dynamodb.loads(servs['Items']):
+                    count = count + 1
+                    if serv['SKID'].replace('SER#','') == item['SERVICEID']:
+                        servName = serv['NAME']
+                if count == 1:
+                    servName = ''
+
+                provs =  dynamodb.query(
+                    TableName="TuCita247",
+                    ReturnConsumedCapacity='TOTAL',
+                    KeyConditionExpression='PKID = :key AND begins_with(SKID , :provs)',
+                    ExpressionAttributeValues={
+                        ':key': {'S': 'BUS#' + item['GSI1PK'].split('#')[1] + '#LOC#'+item['GSI1PK'].split('#')[3]},
+                        ':provs': {'S': 'PRO#' }
+                    }
+                )
+                countp = 0
+                provName = ''
+                for prov in json_dynamodb.loads(provs['Items']):
+                    countp = countp + 1
+                    if prov['SKID'].replace('PRO#','') == item['GSI1PK'].split('#')[5]:
+                        provName = prov['NAME']
+                if countp == 1:
+                    provName = ''
+
+                recordset = {
+                    'AppointmentId': item['PKID'].replace('APPO#',''),
+                    'Status': item['STATUS'],
+                    'Address': Address,
+                    'NameBusiness': Name,
+                    'Name': item['NAME'],
+                    'Phone': item['PHONE'],
+                    'DateAppo': item['DATE_APPO'],
+                    'Door': item['DOOR'] if 'DOOR' in item else '',
+                    'OnBehalf': item['ON_BEHALF'] if 'ON_BEHALF' in item else 0,
+                    'PeopleQty': item['PEOPLE_QTY'] if 'PEOPLE_QTY' in item else 0,
+                    'QRCode': item['QRCODE'] if 'QRCODE' in item else '',
+                    'Disability': item['DISABILITY'] if 'DISABILITY' in item else 0,
+                    'UnRead': item['UNREAD'] if 'UNREAD' in item else '',
+                    'Ready': item['READY'] if 'READY' in item else 0,
+                    'ServName': servName,
+                    'ProvName': provName
+                }
+                record.append(recordset)
 
         record.sort(key=getKey)
         statusCode = 200
