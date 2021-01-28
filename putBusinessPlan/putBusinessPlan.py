61bdf420deb642f8f66b8b342705694e6cb85073
Erick Santizo <esantizo@octagt.com>
2021-01-27 19:03:00 -0600

    changes to API WP

--- putBusinessPlan/putBusinessPlan.py
+++ putBusinessPlan/putBusinessPlan.py
@@ -20,6 +20,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.client('dynamodb', region_name=REGION)
+ses = boto3.client('ses',region_name=REGION)
 logger.info("SUCCESS: Connection to DynamoDB succeeded")
 
 def lambda_handler(event, context):
@@ -35,10 +36,29 @@ def lambda_handler(event, context):
         subId = event['pathParameters']['subId']
 
         dueDate = ''
+        language = ''
+        email = ''
+        phone = ''
+        name = ''
         country_date = dateutil.tz.gettz('America/Puerto_Rico')
         today = datetime.datetime.now(tz=country_date)
         dueDate = (today + datetime.timedelta(days=31)).strftime("%Y-%m-%d")
         
+        lanData = dynamodb.query(
+            TableName="TuCita247",
+            ReturnConsumedCapacity='TOTAL',
+            KeyConditionExpression='PKID = :pkid AND SKID = :skid',
+            ExpressionAttributeValues={
+                ':pkid': {'S': 'BUS#' + businessId},
+                ':skid': {'S': 'METADATA'}
+            }
+        )
+        for lang in json_dynamodb.loads(lanData['Items']):
+            language = lang['LANGUAGE'] if 'LANGUAGE' in lang else 'es'
+            email = lang['EMAIL']
+            phone = lang['PHONE']
+            name = lang['NAME']
+
         dataPlan = dynamodb.query(
             TableName = "TuCita247",
             ReturnConsumedCapacity = 'TOTAL',
@@ -103,6 +123,64 @@ def lambda_handler(event, context):
         logger.info(items)
         response = dynamodb.transact_write_items(
             TransactItems = items
+        )
+
+        SENDER = "Tu Cita 24/7 <no-reply@tucita247.com>"
+        SUBJECT = "Tu Cita 24/7 - Successful payment" if language == 'en' else "Tu Cita 24/7 - Pago exitoso"
+        if language == 'en':
+            BODY_TEXT = ("Successful payment. Business: "+name+" Email: "+email+" Phone number: "+phone+" Plan: "+plan)
+        else:
+            BODY_TEXT = ("Pago exitoso. Negocio: "+name+" Correo electrónico: "+email+" Teléfono: "+phone+" Plan: "+plan)
+        if language == 'en':
+            BODY_HTML = """<html>
+            <head></head>
+            <body>
+            <h1>Successful payment</h1>
+            <p><strong>Business: </strong>""" +name+ """</p>
+            <p><strong>Email: </strong>"""+email+"""</p>
+            <p><strong>Phone number: </strong>""" +phone+ """</p>
+            <p><strong>Plan: </strong>""" +plan+ """</p>
+            </body>
+            </html>"""
+        else:
+            BODY_HTML = """<html>
+            <head></head>
+            <body>
+            <h1>Pago exitoso</h1>
+            <p><strong>Negocio: </strong>""" +name+ """</p>
+            <p><strong>Correo electrónico: </strong>"""+email+"""</p>
+            <p><strong>Teléfono: </strong>""" +phone+ """</p>
+            <p><strong>Plan: </strong>""" +plan+ """</p>
+            </body>
+            </html>"""
+        
+        CHARSET = "UTF-8"
+        response = ses.send_email(
+            Destination={
+                'ToAddresses': [
+                    email
+                ],
+                'BccAddresses': [
+                    'rita@lutorio.com', 'carlos@lutorio.com', 'esantizo@octagt.com'
+                ],
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER
         )
 
         statusCode = 200
--- putCancelPlanWP/putCancelPlanWP.py
+++ putCancelPlanWP/putCancelPlanWP.py
@@ -6,6 +6,7 @@ import boto3
 import botocore.exceptions
 from botocore.exceptions import ClientError
 from boto3.dynamodb.conditions import Key, Attr
+from dynamodb_json import json_util as json_dynamodb
 
 import os
 
@@ -15,6 +16,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodb = boto3.client('dynamodb', region_name=REGION)
+ses = boto3.client('ses',region_name=REGION)
 logger.info("SUCCESS: Connection to DynamoDB succeeded")
 
 def lambda_handler(event, context):
@@ -24,47 +26,129 @@ def lambda_handler(event, context):
     try:
         businessId = event['pathParameters']['businessId']
         order = event['pathParameters']['order']
-
-        items = []
-        rows = {}
-        rows = {
-            "Update": {
-                "TableName": "TuCita247",
-                "Key": {
-                    "PKID": {"S": 'BUS#' + businessId },
-                    "SKID": {"S": 'PLAN' }
-                },
-                "UpdateExpression":"SET #o = :order, #s = :status",
-                "ExpressionAttributeNames":{'#o': 'ORDER','#s': 'STATUS'},
-                "ExpressionAttributeValues": {
-                    ":order": {"S": order},
-                    ":status": {"N": str(0)}
-                },
-                "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
-            },
-        }
-        items.append(rows)
-
-        rows = {
-            "Update": {
-                "TableName": "TuCita247",
-                "Key": {
-                    "PKID": {"S": 'BUS#' + businessId },
-                    "SKID": {"S": 'METADATA' }
-                },
-                "UpdateExpression":"set #s = :status",
-                "ExpressionAttributeValues": { 
-                    ":status": {"N": str(0)}
-                },
-                "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
-            },
-        }
-        items.append(rows)
-
-        logger.info(items)
-        response = dynamodb.transact_write_items(
-            TransactItems = items
+        typeCancel = int(event['pathParameters']['typeCancel'])
+        respCode = int(event['pathParameters']['respCode'])
+        plan = event['pathParameters']['plan']
+ 
+        language = ''
+        email = ''
+        phone = ''
+        name = ''
+        lanData = dynamodb.query(
+            TableName="TuCita247",
+            ReturnConsumedCapacity='TOTAL',
+            KeyConditionExpression='PKID = :pkid AND SKID = :skid',
+            ExpressionAttributeValues={
+                ':pkid': {'S': 'BUS#' + businessId},
+                ':skid': {'S': 'METADATA'}
+            }
         )
+        for lang in json_dynamodb.loads(lanData['Items']):
+            language = lang['LANGUAGE'] if 'LANGUAGE' in lang else 'es'
+            email = lang['EMAIL']
+            phone = lang['PHONE']
+            name = lang['NAME']
+
+        if typeCancel == 2:
+            items = []
+            rows = {}
+            rows = {
+                "Update": {
+                    "TableName": "TuCita247",
+                    "Key": {
+                        "PKID": {"S": 'BUS#' + businessId },
+                        "SKID": {"S": 'PLAN' }
+                    },
+                    "UpdateExpression":"SET #o = :order, #s = :status",
+                    "ExpressionAttributeNames":{'#o': 'ORDER','#s': 'STATUS'},
+                    "ExpressionAttributeValues": {
+                        ":order": {"S": order},
+                        ":status": {"N": str(0)}
+                    },
+                    "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
+                },
+            }
+            items.append(rows)
+
+            rows = {
+                "Update": {
+                    "TableName": "TuCita247",
+                    "Key": {
+                        "PKID": {"S": 'BUS#' + businessId },
+                        "SKID": {"S": 'METADATA' }
+                    },
+                    "UpdateExpression":"set #s = :status",
+                    "ExpressionAttributeValues": { 
+                        ":status": {"N": str(0)}
+                    },
+                    "ReturnValuesOnConditionCheckFailure": "ALL_OLD"
+                },
+            }
+            items.append(rows)
+
+            logger.info(items)
+            response = dynamodb.transact_write_items(
+                TransactItems = items
+            )
+        
+        if respCode == 1:
+            SENDER = "Tu Cita 24/7 <no-reply@tucita247.com>"
+            SUBJECT = "Tu Cita 24/7 - Payment failed" if language == 'en' else "Tu Cita 24/7 - Pago fallido"
+            if language == 'en':
+                BODY_TEXT = ("Payment failed. Business: "+name+" Email: "+email+" Phone number: "+phone+" Plan: "+plan)
+            else:
+                BODY_TEXT = ("Pago fallido. Negocio: "+name+" Correo electrónico: "+email+" Teléfono: "+phone+" Plan: "+plan)
+            if language == 'en':
+                BODY_HTML = """<html>
+                <head></head>
+                <body>
+                <h1>Payment failed</h1>
+                <p><strong>Business: </strong>""" +name+ """</p>
+                <p><strong>Email: </strong>"""+email+"""</p>
+                <p><strong>Phone number: </strong>""" +phone+ """</p>
+                <p><strong>Plan: </strong>""" +plan+ """</p>
+                </body>
+                </html>"""
+            else:
+                BODY_HTML = """<html>
+                <head></head>
+                <body>
+                <h1>Pago fallido</h1>
+                <p><strong>Negocio: </strong>""" +name+ """</p>
+                <p><strong>Correo electrónico: </strong>"""+email+"""</p>
+                <p><strong>Teléfono: </strong>""" +phone+ """</p>
+                <p><strong>Plan: </strong>""" +plan+ """</p>
+                </body>
+                </html>"""
+            
+            CHARSET = "UTF-8"
+            response = ses.send_email(
+                Destination={
+                    'ToAddresses': [
+                        email
+                    ],
+                    'BccAddresses': [
+                        'rita@lutorio.com', 'carlos@lutorio.com', 'esantizo@octagt.com'
+                    ],
+                },
+                Message={
+                    'Body': {
+                        'Html': {
+                            'Charset': CHARSET,
+                            'Data': BODY_HTML,
+                        },
+                        'Text': {
+                            'Charset': CHARSET,
+                            'Data': BODY_TEXT,
+                        },
+                    },
+                    'Subject': {
+                        'Charset': CHARSET,
+                        'Data': SUBJECT,
+                    },
+                },
+                Source=SENDER
+            )
 
         statusCode = 200
         body = json.dumps({'Message': 'Business updated successfully', 'Code': 200})
